bf5eee19179009b7c76a8d262e024f5731c2c5ff
Adam E Nagy <adam.nagy@autodesk.com>
2025-04-10 20:43:03 +0100

    Remove unused callback endpoint

--- designAutomationSample/Controllers/DesignAutomationController.cs
+++ designAutomationSample/Controllers/DesignAutomationController.cs
@@ -431,51 +431,6 @@ namespace designAutomationSample.Controllers
         }
 
 
-
-        /// <summary>
-        /// Callback from Design Automation Workitem (onProgress or onComplete)
-        /// </summary>
-        [HttpPost]
-        [Route("/api/aps/callback/designautomation")]
-        public async Task<IActionResult> OnCallback(string id, string outputFileName, [FromBody] dynamic body)
-        {
-            try
-            {
-                // your webhook should return immediately! we can use Hangfire to schedule a job
-                JObject bodyJson = JObject.Parse((string)body.ToString());
-                await _hubContext.Clients.Client(id).SendAsync("onComplete", bodyJson.ToString());
-
-                using (var httpClient = new HttpClient())
-                {
-                    byte[] bs = await httpClient.GetByteArrayAsync(bodyJson["reportUrl"]?.Value<string>());
-                    string report = System.Text.Encoding.Default.GetString(bs);
-                    await _hubContext.Clients.Client(id).SendAsync("onComplete", report);
-                }
-
-                // OAuth token
-                dynamic oauth = await OAuthController.GetInternalAsync();
-
-                ObjectsApi objectsApi = new ObjectsApi();
-                objectsApi.Configuration.AccessToken = oauth.access_token;              
-
-                ApiResponse<dynamic> res = await objectsApi.getS3DownloadURLAsyncWithHttpInfo(
-                                            NickName.ToLower()+"-designautomation",
-                                            outputFileName, new Dictionary<string, object> {
-                                            { "minutesExpiration", 15.0 },
-                                            { "useCdn", true }
-                                            });
-                await _hubContext.Clients.Client(id).SendAsync("downloadResult", (string)(res.Data.url));
-                Console.WriteLine("Congrats!");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            // ALWAYS return ok (200)
-            return Ok();
-        }
-
         /// <summary>
         /// Clear the accounts (for debugging purposes)
         /// </summary>
