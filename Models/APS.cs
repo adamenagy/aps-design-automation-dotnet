d3675da9e6c3cffbc76e3dfe4add5b6cc5d7a93e
Adam E Nagy <adam_e_nagy@hotmail.com>
2026-07-02 11:26:02 +0100

    remove pat

--- Models/APS.cs
+++ Models/APS.cs
@@ -3,16 +3,14 @@
     private readonly string _clientId;
     private readonly string _clientSecret;
     private readonly string _nickname;
-    private readonly string _pat;
     private readonly string _bucket;
     private readonly string _alias;
 
-    public APS(string clientId, string clientSecret, string? nickname = null, string? bucket = null, string? pat = null)
+    public APS(string clientId, string clientSecret, string? nickname = null, string? bucket = null)
     {
         _clientId = clientId;
         _clientSecret = clientSecret;
         _nickname = (nickname != null) ? nickname : _clientId;
-        _pat = (pat != null) ? pat : "";
         _bucket = (bucket != null) ? bucket : _nickname.ToLower() + "-designautomation";
         _alias = "dev";
     }
